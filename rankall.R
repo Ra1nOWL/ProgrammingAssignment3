193cda0774324a2b9586ab8e06280c4199caa61a
Ra1nOWL <oonlythone@gmail.com>
2020-05-16 16:14:01 +0800

    Updated rankall.R

--- rankall.R
+++ rankall.R
@@ -1,45 +1,52 @@
 rankall <- function(outcome, num = "best") {
+  ## Read outcome data
+  data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  data <- data[c(2, 7, 11, 17, 23)]
+  names(data)[1] <- "name"
+  names(data)[2] <- "state"
+  names(data)[3] <- "heart attack"
+  names(data)[4] <- "heart failure"
+  names(data)[5] <- "pneumonia"
   
-  # Read outcome data
-  out_dt <- data.table::fread('outcome-of-care-measures.csv')
+  ## Validate the outcome string
+  outcomes = c("heart attack", "heart failure", "pneumonia")
+  if( outcome %in% outcomes == FALSE ) stop("invalid outcome")
   
-  outcome <- tolower(outcome)
+  ## Validate the num value
+  if( num != "best" && num != "worst" && num%%1 != 0 ) stop("invalid num")
   
-  if (!outcome %in% c("heart attack", "heart failure", "pneumonia")) {
-    stop('invalid outcome')
+  ## Grab only rows with data in our outcome
+  data <- data[data[outcome] != 'Not Available', ]
+  
+  ## Order the data
+  data[outcome] <- as.data.frame(sapply(data[outcome], as.numeric))
+  data <- data[order(data$name, decreasing = FALSE), ]
+  data <- data[order(data[outcome], decreasing = FALSE), ]
+  
+  ## Helper functiont to process the num argument
+  getHospByRank <- function(df, s, n) {
+    df <- df[df$state==s, ]
+    vals <- df[, outcome]
+    if( n == "best" ) {
+      rowNum <- which.min(vals)
+    } else if( n == "worst" ) {
+      rowNum <- which.max(vals)
+    } else {
+      rowNum <- n
+    }
+    df[rowNum, ]$name
   }
   
-  # Renaming Columns to be less verbose and lowercase
-  setnames(out_dt
-           , tolower(sapply(colnames(out_dt), gsub, pattern = "^Hospital 30-Day Death \\(Mortality\\) Rates from ", replacement = "" ))
-  )
-  
-  # Columns indices to keep
-  col_indices <- grep(paste0("hospital name|state|^",outcome), colnames(out_dt))
-  
-  # Filtering out unnessecary data 
-  out_dt <- out_dt[, .SD ,.SDcols = col_indices]
-  
-  # Find out what class each column is 
-  # sapply(out_dt,class)
-  
-  # Change outcome column class
-  out_dt[, outcome] <- out_dt[,  as.numeric(get(outcome))]
-  
-  if (num == "best"){
-    return(out_dt[order(state, get(outcome), `hospital name`)
-                  , .(hospital = head(`hospital name`, 1))
-                  , by = state])
+  ## For each state, find the hospital of the given rank
+  states <- data[, 2]
+  states <- unique(states)
+  newdata <- data.frame("hospital"=character(), "state"=character())
+  for(st in states) {
+    hosp <- getHospByRank(data, st, num)
+    newdata <- rbind(newdata, data.frame(hospital=hosp, state=st))
   }
   
-  if (num == "worst"){
-    return(out_dt[order(get(outcome), `hospital name`)
-                  , .(hospital = tail(`hospital name`, 1))
-                  , by = state])
-  }
-  
-  return(out_dt[order(state, get(outcome), `hospital name`)
-                , head(.SD,num)
-                , by = state, .SDcols = c("hospital name") ])
-  
+  ## Return a data frame with the hospital names and the (abbreviated) state name
+  newdata <- newdata[order(newdata['state'], decreasing = FALSE), ]
+  newdata
 }
